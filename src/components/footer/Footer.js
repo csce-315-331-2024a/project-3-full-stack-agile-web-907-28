7b4c5609cc98f24cc59c18c189be3026d0fde1b9
Alexis Rappa <35510342+arappa@users.noreply.github.com>
2024-03-28 18:31:24 -0500

    Implementing the footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import { useSession } from 'next-auth/react';
+import styles from '../../styles/footer/Footer.module.css';
+
+const Footer = () => {
+    return (
+      <footer className={styles.footer}>
+        {}
+      </footer>
+    );
+  };
+  
+  export default Footer;
