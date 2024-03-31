f76508ec0f5af05eedc57f710aea5706a237c62d
Alexis Rappa <35510342+arappa@users.noreply.github.com>
2024-03-31 16:46:41 -0500

    Implementing footer for use by all pages

---
+++ _document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Footer from '@/components/footer/Footer'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      <Footer />
+    </Html>
+  )
+}
