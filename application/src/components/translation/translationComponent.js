9293afbffafdb959c2e6aacf426ee55c32c90815
CreeKeem <388cjk@gmail.com>
2024-04-08 22:39:31 -0500

    tried to fix the bug
    the bug shows the language selector twice, tried to get rid of it

--- application/src/components/translation/translationComponent.js
+++ application/src/components/translation/translationComponent.js
@@ -4,21 +4,29 @@ import React, { useEffect } from 'react';
 
 const GoogleTranslate = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-    script.async = true;
+    // Check if the script is already present
+    if (!document.querySelector('#google-translate-script')) {
+      const script = document.createElement('script');
+      script.id = 'google-translate-script';
+      script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+      script.async = true;
 
-    script.onload = () => {
-      const googleTranslateElementInit = () => {
-        new window.google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+      script.onload = () => {
+        const googleTranslateElementInit = () => {
+          new window.google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+        };
+        window.googleTranslateElementInit = googleTranslateElementInit;
       };
-      window.googleTranslateElementInit = googleTranslateElementInit;
-    };
 
-    document.body.appendChild(script);
+      document.body.appendChild(script);
+    }
 
+    // Cleanup function
     return () => {
-      document.body.removeChild(script);
+      const script = document.querySelector('#google-translate-script');
+      if (script) {
+        document.body.removeChild(script);
+      }
     };
   }, []);
 
@@ -27,4 +35,4 @@ const GoogleTranslate = () => {
   );
 };
 
-export default GoogleTranslate;
+export default React.memo(GoogleTranslate);
