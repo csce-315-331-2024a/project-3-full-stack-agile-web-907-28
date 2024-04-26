b66b17548c3b1dbd3100505fa6314559a8d4f4d9
jeffslobodkin <jeffs7212@gmail.com>
2024-04-26 14:54:26 -0500

    Debugging production issue with customner query

--- application/src/pages/api/customer/getCustomers.js
+++ application/src/pages/api/customer/getCustomers.js
@@ -3,8 +3,11 @@ import Customer from '@/models/Customer';
 
 export default async function handler(req, res) {
   try {
+    console.log("Getting customers");
     const { rows } = await query('SELECT * FROM customers;');
+    console.log("Customers: ", rows);
     const customers = rows.map(Customer.parseDatabaseEntry);
+    console.log("Customers after map: ", customers);
     return res.status(200).json(customers);
   } catch (error) {
     console.error(error);
